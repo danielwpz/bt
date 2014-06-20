1e34376afc33a3cd4a181372e07d85564c235cca
Daniel Wang <danielwpz@gmail.com>
2014-06-20 19:17:07 +0800

    test/peer

--- test.c
+++ test.c
@@ -4,6 +4,8 @@
  * Authors: Daniel
  *			Florrie
  *
+ * THIS IS A TEST FILE.
+ * 
  * Date:	2014-6
  */
 
@@ -26,18 +28,22 @@
 
 extern int kfd;
 
+/* for test */
+int cmdType = 0;
+
 void peer_run(bt_config_t *config);
 
+/* TEST DRIVER */
 int main(int argc, char **argv) {
 	bt_config_t config;
 
 	bt_init(&config, argc, argv);
-	
+
 	debug = DEBUG_ALL;
 	DPRINTF(DEBUG_INIT, "peer.c main beginning\n");
 
 #ifdef TESTING
-	config.identity = 4; // your group number here
+	config.identity = 1; // your group number here
 	strcpy(config.chunk_file, "chunkfile");
 	strcpy(config.has_chunk_file, "haschunks");
 #endif
@@ -51,7 +57,14 @@ int main(int argc, char **argv) {
 #endif
 
 	/* TODO: fill in peers num and local port */
-	send_init(0, 11234);
+	int port;
+	int ret;
+	scanf("%d", &port);
+	ret = send_init(4, (short)port);
+	if (ret < 0) {
+		Debug("send_init error %d\n", ret);
+	}
+	Debug("start!\n");
 
 	peer_run(&config);
 	return 0;
@@ -74,11 +87,15 @@ void process_inbound_udp(int sock) {
 			buf);
 }
 
+void anounce_timeout(int interval) {
+	process_timer(interval);
+}
 
 void peer_run(bt_config_t *config) {
 	int n;
 	struct sockaddr_in myaddr;
 	fd_set readfds;
+	struct timeval tv;
 	struct user_iobuf *userbuf;
 	char buf[BUF_SIZE];
 
@@ -92,28 +109,66 @@ void peer_run(bt_config_t *config) {
 	myaddr.sin_family = AF_INET;
 	myaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	myaddr.sin_port = htons(config->myport);
-	//spiffy_init(config->identity, (struct sockaddr *)&myaddr, sizeof(myaddr));
+	spiffy_init(config->identity, (struct sockaddr *)&myaddr, sizeof(myaddr));
 
-	hd_init(config, htonl(INADDR_ANY));
 	while (1) {
 		int nfds;
+		FD_ZERO(&readfds);
 		FD_SET(STDIN_FILENO, &readfds);
 		FD_SET(kfd, &readfds);
 
-		nfds = select(kfd+1, &readfds, NULL, NULL, NULL);
+		tv.tv_sec = 1;
+		tv.tv_usec = 0;
+
+		nfds = select(kfd+1, &readfds, NULL, NULL, &tv);
 
 		if (nfds > 0) {
+
 			if (FD_ISSET(kfd, &readfds)) {
 				process_udp(kfd);
 			}
 
 			if (FD_ISSET(STDIN_FILENO, &readfds)) {
-				n = read(STDIN_FILENO, buf, BUF_SIZE);
-				if (n > 0) {
-					buf[n] = '\0';
-					handle_cmd(buf);
+				int i, ret;
+				in_addr_t dIP = inet_addr("127.0.0.1");
+				short dport;
+				char buf[1024];
+				char data[1024];
+				ret = read(STDIN_FILENO, data, 1024);
+				data[ret] = '\0';
+				sscanf(data, "%d %s", &dport, buf);
+				char *tmpbuf = malloc(512 * 1024);
+
+				for (i = 0; i < strlen(buf); i++) {
+					tmpbuf[i] = buf[i];
+				}
+				for (; i < 512 * 1024; i++) {
+					tmpbuf[i] = 0;
+				}
+
+				ret = send_data(dIP, dport, tmpbuf, 512 * 1024);
+
+				/*
+				cmdType = (cmdType + 1) % 4;
+
+				if (cmdType == 1) {
+					ret = send_whohas(dIP, dport, buf, strlen(buf));
+				}else if (cmdType == 2) {
+					ret = send_ihave(dIP, dport, buf, strlen(buf));
+				}else if (cmdType == 3) {
+					ret = send_get(dIP, dport, buf, strlen(buf));
+				}else if (cmdType == 0) {
+					ret = send_data(dIP, dport, buf, strlen(buf));
+				}
+				*/
+
+				if (ret < 0) {
+					printf("error %d\n", ret);
 				}
 			}
+			anounce_timeout(SELECT_TICK);
+		}else if (nfds == 0) {
+			anounce_timeout(SECOND_TICK);
 		}
 	}
 }
